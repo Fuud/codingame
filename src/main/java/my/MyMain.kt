3630d3e9908ab114e153f2b87184a67dd1f01850
Fedor Bobin <fuudtorrentsru@gmail.com>
2022-02-24 21:22:05 +0300

    parse

--- src/main/java/my/MyMain.kt
+++ src/main/java/my/MyMain.kt
@@ -4,10 +4,56 @@ object MyMain {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        println("it works")
+        println(parse(Task.a_an_example))
     }
 }
 
-data class User(val name: String, val skills: Map<String, Long>)
+data class User(val name: String, val skills: Map<String, Int>)
 
-data class Project(val days: Long, val score: Long, val bestBefore: Long, val roleToLevel: Map<String, Long>)
+data class Project(val name: String, val days: Int, val score: Int, val bestBefore: Int, val roleToLevel: Map<String, Int>)
+
+data class UsersAndProjects(val users: List<User>, val projects: List<Project>)
+
+enum class Task{
+    a_an_example,
+    b_better_start_small,
+    c_collaboration,
+    d_dense_schedule,
+    e_exceptional_skills,
+    f_find_great_mentors,
+}
+
+fun parse(file: Task): UsersAndProjects{
+     mutableListOf<User>()
+    val projects = mutableListOf<Project>()
+
+    MyMain::class.java.classLoader.getResourceAsStream("${file}.in.txt").bufferedReader().use {
+        val (contributorsCount, projectsCount) = it.readLine().split(" ").map { it.toInt() }
+        val users = (0 until contributorsCount).map{_ ->
+            val contrLn = it.readLine()
+            val name = contrLn.substringBeforeLast(" ")
+            val skillCount = contrLn.substringAfterLast(" ").toInt()
+            val skills = (0 until skillCount).map{_ ->
+                val skillLn = it.readLine()
+                val skillName = skillLn.substringBeforeLast(" ")
+                val skillLevel = skillLn.substringAfterLast(" ").toInt()
+                skillName to skillLevel
+            }.toMap()
+            User(name, skills)
+        }
+        val projects = (0 until projectsCount).map {_ ->
+            val projectLn = it.readLine()
+            val projectParts = projectLn.split(" ")
+            val name = projectParts.dropLast(4).joinToString(separator = " ")
+            val (days, score, bestBefore, rolesCount) = projectParts.takeLast(4).map { it.toInt() }
+            val roleToLevel = (0 until rolesCount).map { _ ->
+                val skillLn = it.readLine()
+                val skillName = skillLn.substringBeforeLast(" ")
+                val skillLevel = skillLn.substringAfterLast(" ").toInt()
+                skillName to skillLevel
+            }.toMap()
+            Project(name, days, score, bestBefore, roleToLevel)
+        }
+        return UsersAndProjects(users, projects)
+    }
+}
