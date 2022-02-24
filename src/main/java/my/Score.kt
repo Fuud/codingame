04815f5d03b9c111cca54abd5fd5bdf423f33fe4
Anton Chistyakov <Anton.Chistyakov@fxdd.com>
2022-02-24 21:28:22 +0300

    out

---
+++ src/main/java/my/Score.kt
@@ -0,0 +1,39 @@
+package my
+
+import java.io.File
+
+data class Score(val taskName: String) {
+    var score = 0L
+    var day = 0
+    private val projectOuts = mutableListOf<ProjectOut>()
+
+    data class ProjectOut(val project: Project, val users: List<User>) {
+    }
+
+    fun addProject(project: Project, users: List<User>): Boolean {
+        val projectEnd = day + project.days
+        if (projectEnd <= project.bestBefore) {
+            score += score + project.score
+        } else {
+            val scoreAdd = project.score - (projectEnd - project.bestBefore)
+            if (scoreAdd < 0) {
+                return false
+            }
+            score += scoreAdd
+        }
+        projectOuts.add(ProjectOut(project, users))
+        return true
+    }
+
+    fun print() {
+        File("${taskName}.${score}.${System.currentTimeMillis()}.txt").printWriter().use { writer ->
+            writer.println(projectOuts.size)
+            projectOuts.forEach {
+                writer.println(it.project.name)
+                val userString = it.users.joinToString(separator = " ") { u -> u.name }
+                writer.print(userString)
+            }
+
+        }
+    }
+}
