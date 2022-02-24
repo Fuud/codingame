646dde08d361eb81b59919681d2580a7103cc351
Anton Chistyakov <Anton.Chistyakov@fxdd.com>
2022-02-24 22:30:04 +0300

    out

--- src/main/java/my/Score.kt
+++ src/main/java/my/Score.kt
@@ -2,6 +2,21 @@ package my
 
 import java.io.File
 
+fun main(string: Array<String>) {
+    val score = Score("A")
+    val ann = User("Anna", mutableMapOf("C++" to 2))
+    val bob = User("Bob", mutableMapOf("HTML" to 5, "CSS" to 5))
+    val maria = User("Maria", mutableMapOf("Python" to 3))
+
+    val logging = Project("Logging", 5, 10, 5, mutableMapOf())
+    val webServer = Project("WebServer", 7, 10, 7, mutableMapOf())
+    val webChat = Project("WebChat", 10, 20, 20, mutableMapOf())
+    score.print(projectOutList = listOf(Score.ProjectOut(webServer, listOf(bob, ann)),
+        Score.ProjectOut(logging, listOf(ann)),
+        Score.ProjectOut(webChat, listOf(maria, bob))
+    ))
+}
+
 data class Score(val taskName: String) {
 
     data class ProjectOut(val project: Project, val users: List<User>) {
@@ -14,25 +29,27 @@ data class Score(val taskName: String) {
         projectOutList.forEach { it ->
             // find day to start project
             val day2start = it.users.map { u -> user2day[u] ?: 0 }.max() ?: 0
-            val endDay = day2start + it.project.bestBefore
+            val endDay = day2start + it.project.days - 1
             if (endDay < it.project.bestBefore) {
                 score += it.project.score
             } else {
-                val scoreAdd = it.project.bestBefore - (endDay + 1 - it.project.bestBefore)
+                val scoreAdd = it.project.score - (endDay + 1 - it.project.bestBefore)
                 if (scoreAdd > 0) {
                     score += scoreAdd
                 }
             }
-            it.users.forEach { user -> user2day[user] = endDay }
+            it.users.forEach { user -> user2day[user] = endDay + 1 }
         }
 
-        File("${taskName}.${score}.${System.currentTimeMillis()}.txt").printWriter().use { writer ->
+        File("${taskName}.${score}.txt").printWriter().use { writer ->
             writer.println(projectOutList.size)
             projectOutList.forEach {
                 writer.println(it.project.name)
                 val userString = it.users.joinToString(separator = " ") { u -> u.name }
                 writer.print(userString)
+                writer.println()
             }
         }
     }
+
 }
