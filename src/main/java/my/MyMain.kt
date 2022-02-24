85a10835c4770aedf874c6ec27b434a9baa91dce
Fedor Bobin <fuudtorrentsru@gmail.com>
2022-02-24 21:00:34 +0300

    dto

--- src/main/java/my/MyMain.kt
+++ src/main/java/my/MyMain.kt
@@ -7,3 +7,7 @@ object MyMain {
         println("it works")
     }
 }
+
+data class User(val name: String, val skills: Map<String, Long>)
+
+data class Project(val days: Long, val score: Long, val bestBefore: Long, val roleToLevel: Map<String, Long>)
